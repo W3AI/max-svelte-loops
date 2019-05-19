c69ef2d04633db18012b392cf6f3a217d4824a7d
stefian <v_ianta@yahoo.com>
2019-05-19 17:17:29 -0400

    done first 3 points

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,40 @@
+<script>
+
+let pass = '';
+let passState = '';
+
+let passList = [];
+
+$: if (pass.trim().length < 5) {
+        passState = 'Too short';
+    } else if (pass.trim().length > 10) {
+        passState = 'Too long';
+    } else {
+        passState = 'ok';
+    }
+
+</script>
+
 <h1>Assignment</h1>
-
 <p>Solve these tasks.</p>
-
 <ol>
-	<li>Add a password input field and save the user input in a variable.</li>
-	<li>Output "Too short" if the password is shorter than 5 characters and "Too long" if it's longer than 10.</li>
-	<li>Output the password in a paragraph tag if it's between these boundaries.</li>
+	<li>&#10003; - Add a password input field and save the user input in a variable.</li>
+	<li>&#10003; - Output "Too short" if the password is shorter than 5 characters and "Too long" if it's longer than 10.</li>
+	<li>&#10003; - Output the password in a paragraph tag if it's between these boundaries.</li>
 	<li>Add a button and let the user add the passwords to an array.</li>
 	<li>Output the array values (= passwords) in a unordered list (ul tag).</li>
 	<li>Bonus: If a password is clicked, remove it from the list.</li>
 </ol>
+<hr>
+<label>Password:</label>
+<input type="text" bind:value={pass} id="pass"/>
+
+{#if passState === 'Too short'}
+    <p>Too short</p>
+{/if}
+{#if passState === 'ok'}
+    <p>{pass}</p>
+{/if}
+{#if passState === 'Too long'}
+    <p>Too long</p>
+{/if}
